311f136af5d2d4c9b65ee97f23eafacad20b888d
arocoding <arocoding@gmail.com>
2022-02-26 18:26:43 +0900

    gulp-sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from 'gulp-pug';
 import del from 'del';
 import ws from 'gulp-webserver';
 import image from 'gulp-image';
+import gSass from 'gulp-sass';
+import nSass from 'node-sass';
+const sass = gSass(nSass);
 
 const routes = {
   pug: {
@@ -12,6 +15,10 @@ const routes = {
   img: {
     src: 'src/img/*',
     dest: 'build/img',
+  },
+  scss: {
+    src: 'src/scss/style.scss',
+    dest: 'build/css',
   },
 };
 
@@ -26,8 +33,14 @@ const webserver = () =>
 const img = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 const live = gulp.parallel([webserver]);
 
 export const dev = gulp.series([prepare, assets, live]);
